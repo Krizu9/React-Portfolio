7026dba47d4b21e5247590da25906eacf3851777
Krizu9 <kristian.pekkanen6@gmail.com>
2024-08-15 19:54:45 +0300

    fixed error

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Footer from './components/Footer';
 import App from './App.jsx';
-import Navbar from './components/Navbar.jsx';
+import Navbar from './components/navbar.jsx';
 import './styles/index.css';
 
 createRoot(document.getElementById('root')).render(
